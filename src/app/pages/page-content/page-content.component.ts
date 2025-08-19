b2c7533b411f2a33d9cda889ef705bbc7e935c70
buccaneer33 <buccaneer33@inbox.ru>
2025-08-19 14:49:46 +0400

    [FINISH]: 004 zoom done

--- src/app/pages/page-content/page-content.component.ts
+++ src/app/pages/page-content/page-content.component.ts
@@ -1,4 +1,15 @@
-import { AfterContentInit, Component, input, output } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  computed,
+  effect,
+  input,
+  OnDestroy,
+  output,
+  untracked,
+  ViewChild
+} from '@angular/core';
+import { PinchZoomComponent } from '@meddv/ngx-pinch-zoom';
 
 @Component({
   selector: 'app-page-content',
@@ -6,13 +17,42 @@ import { AfterContentInit, Component, input, output } from '@angular/core';
   styleUrl: './page-content.component.scss',
   standalone: false,
 })
-export class PageContentComponent implements AfterContentInit {
-  readonly id = input.required<number>()
-  readonly idPrefix = input.required<string>()
-  readonly content = input.required<string>()
-  readonly isReady = output<number>()
+export class PageContentComponent implements AfterContentInit, OnDestroy {
+  readonly id = input.required<number>();
+  readonly idPrefix = input.required<string>();
+  readonly content = input.required<string>();
+  readonly scale = input.required<number>();
+  readonly currentPage = input.required<number>();
+
+  private prevScale = 0;
+
+  readonly isReady = output<number>();
+
+  @ViewChild(PinchZoomComponent) pinchZoom: PinchZoomComponent | undefined;
+
+  scaleImg = effect(() => {
+    const currScale = this.scale();
+    untracked(() => {
+      if(this.currentPage() !== this.id()){ return; }
+      if(currScale !== undefined && this.pinchZoom){
+        const scaleStep = currScale === 0 ? 0 : Math.abs(currScale) / 10;
+        if(currScale === 0) {
+          this.pinchZoom.toggleZoom();
+        } else
+        if(currScale > this.prevScale){
+          this.pinchZoom.zoomIn(scaleStep);
+        } else {
+          this.pinchZoom.zoomOut(scaleStep);
+        }
+        this.prevScale = currScale;
+      }
+    })
+  })
 
   ngAfterContentInit(): void {
-    this.isReady.emit(this.id())
+    this.isReady.emit(this.id());
+  }
+  ngOnDestroy(): void {
+    this.pinchZoom && this.pinchZoom.destroy();
   }
 }
